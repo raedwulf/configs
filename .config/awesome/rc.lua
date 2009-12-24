38123ea88c65a9ab986da37a419706f3c0dba01b
Ralph Eastwood <tcmreastwood@gmail.com>
2009-12-24 14:18:49 +0000

    Changed awesome layout to be fair

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -55,7 +55,7 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[5])
 end
 -- }}}
 
